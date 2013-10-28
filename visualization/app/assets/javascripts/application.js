247071e4f8b58deb1cb9600d167814dc1b69a3e8
Lucas Jenss <lucas@x3ro.de>
2013-10-28 16:59:03 +0100

    Add basic UI elements to the dashboard layout

--- visualization/app/assets/javascripts/application.js
+++ visualization/app/assets/javascripts/application.js
@@ -13,4 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require ../../../vendor/assets/javascripts/bootstrap.js
+//
 //= require_tree .
--- visualization/app/helpers/application_helper.rb
+++ visualization/app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def icon(icon)
+    raw "<span class='fs1' aria-hidden='true' data-icon='#{icon}'></span>"
+  end
+
 end
