a3c7f73b0cf90107c5583c4cbf6af330297eed0c
Stephan P <stephan.phieler@haw-hamburg.de>
2013-11-07 17:57:23 +0100

    [DA] added getTweets and dumpToConsole methodes to DatabaseAdapter

--- analysis/DataAnalysis/src/DatabaseAdapter.java
+++ analysis/DataAnalysis/src/DatabaseAdapter.java
@@ -1,7 +1,7 @@
-import java.io.FileWriter;
-import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.Timestamp;
+import java.util.ArrayList;
 
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.Statement;
@@ -10,14 +10,12 @@ public class DatabaseAdapter {
 
 	private Connection connect;
 	private Statement statement;
-	// private PreparedStatement preparedStatement;
 	private ResultSet resultSet;
 	private String host;
 	private String port;
 	private String database;
 	private String user;
 	private String password;
-	private Date lastCall;
 
 	public DatabaseAdapter(String host, String port, String database, String user, String password) {
 		this.host = host;
@@ -25,107 +23,70 @@ public class DatabaseAdapter {
 		this.database = database;
 		this.user = user;
 		this.password = password;
-		lastCall = new Date(0);
 	}
 
 	/**
-	 * returns Tweets between last call and now
+	 * Returns Tweets in given time range t1 to t2
 	 */
-	public void getTweets() {
+	public SocialMessage[] getTweets(String table, Timestamp t1, Timestamp t2) {
+		ArrayList<SocialMessage> messages = new ArrayList<SocialMessage>();
+
 		try {
 			open();
+			statement = (Statement) connect.createStatement();
+			resultSet = statement.executeQuery("select id, text, timestamp from " + database + "."
+					+ table + " where timestamp >= '" + t1 + "' and timestamp <= '" + t2 + "'" );
 
-			
-			lastCall.setTime(System.currentTimeMillis());
+			while (resultSet.next()) {
+				SocialMessage sm = new SocialMessage();
+				sm.setId(resultSet.getBigDecimal(1));
+				sm.setText(resultSet.getString(2));
+				sm.setTimestamp(resultSet.getTimestamp(3));
+				messages.add(sm);
+			}
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			close();
 		}
+
+		return messages.toArray(new SocialMessage[messages.size()]);
 	}
 
 	public void storeTweetInformations() {
 
 	}
 
-	public void readDataBase() throws Exception {
-		FileWriter writer;
-
+	/**
+	 * Dumps all messages in given database
+	 */
+	public void dumpToConsole() {
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			connect = (Connection) DriverManager
-					.getConnection("jdbc:mysql://85.25.155.25:3306/MI_WS1314?"
-							+ "user=MIWS1314&password=Oq1gk28@");
-
-			// Statements allow to issue SQL queries to the database
+			open();
 			statement = (Statement) connect.createStatement();
-			// Result set get the result of the SQL query
 			resultSet = statement.executeQuery("select * from MI_WS1314.tweets_sturm");
-//			resultSet.next();
+
 			while (resultSet.next()) {
-
-				// File file = new
-				// File("C:/Users/steph_000/Desktop/training/Sturm/"
-				// + resultSet.getBigDecimal(1) + ".txt");
-				// writer = new FileWriter(file, true);
-				// writer.write(resultSet.getString(2));
-				// writer.flush();
-				// writer.close();
-
-//				 System.out.println("[ID] " + resultSet.getBigDecimal(1));
-//				 System.out.println("[iteration]" + resultSet.getInt(2));
-//				 System.out.println("[text]" + resultSet.getString(3));
-//				 System.out.println("[hashtags]" + resultSet.getString(4));
-//				 System.out.println("[author]" + resultSet.getString(5));
-//				 System.out.println("[retweets]" + resultSet.getInt(6));
-				 System.out.println("[timestamp]" + resultSet.getTimestamp(7).getTime());
-//				 System.out.println("[follower]" + resultSet.getInt(8));
-//				 System.out.println("[geolocation]" + resultSet.getString(9));
-//				 System.out.println("[place]" + resultSet.getString(10));
-//				 System.out.println("[commentRef]" +
-//				 resultSet.getBigDecimal(11));
-				 System.out.println("###########################################################");
-				// i++;
+				System.out.println("[ID] " + resultSet.getBigDecimal(1));
+				System.out.println("[iteration]" + resultSet.getInt(2));
+				System.out.println("[text]" + resultSet.getString(3));
+				System.out.println("[hashtags]" + resultSet.getString(4));
+				System.out.println("[author]" + resultSet.getString(5));
+				System.out.println("[retweets]" + resultSet.getInt(6));
+				System.out.println("[timestamp]" + resultSet.getTimestamp(7).getTime());
+				System.out.println("[follower]" + resultSet.getInt(8));
+				System.out.println("[geolocation]" + resultSet.getString(9));
+				System.out.println("[place]" + resultSet.getString(10));
+				System.out.println("[commentRef]" + resultSet.getBigDecimal(11));
+				System.out.println("###########################################################");
 			}
-
-			// System.out.println("Anzahl: " + i);
-
-			// writeResultSet(resultSet);
-
-			// PreparedStatements can use variables and are more efficient
-			// preparedStatement = (PreparedStatement) connect
-			// .prepareStatement("insert into  TEST.INFORMATION values (?, ?, ?)");
-			// "myuser, webpage, datum, summary, COMMENTS from FEEDBACK.COMMENTS");
-			// Parameters start with 1
-			// preparedStatement.setInt(1, 0);
-			// preparedStatement.setString(2, "TestEmail");
-			// preparedStatement.setInt(3, 123);
-			// preparedStatement.setDate(4, new java.sql.Date(2009, 12, 11));
-			// preparedStatement.setString(5, "TestSummary");
-			// preparedStatement.setString(6, "TestComment");
-			// preparedStatement.executeUpdate();
-
-			// preparedStatement = (PreparedStatement) connect
-			// .prepareStatement("SELECT myuser, webpage, datum, summary, COMMENTS from TEST.COMMENTS");
-			// resultSet = preparedStatement.executeQuery();
-			// writeResultSet(resultSet);
-
-			// Remove again the insert comment
-			// preparedStatement = (PreparedStatement) connect
-			// .prepareStatement("delete from TEST.INFORMATION where id= ? ; ");
-			// preparedStatement.setInt(1, 0);
-			// preparedStatement.executeUpdate();
-			//
-			// resultSet =
-			// statement.executeQuery("select * from TEST.COMMENTS");
-			// writeMetaData(resultSet);
-
+			
 		} catch (Exception e) {
-			throw e;
+			e.printStackTrace();
 		} finally {
 			close();
 		}
-
 	}
 
 	private void open() throws Exception {
--- analysis/DataAnalysis/src/TwitterDataAnalyser.java
+++ analysis/DataAnalysis/src/TwitterDataAnalyser.java
@@ -1,10 +1,21 @@
+import java.sql.Timestamp;
 
 public class TwitterDataAnalyser {
 
 	public static void main(String[] args) {
-		
+
 		try {
-			new DatabaseAdapter("85.25.155.25","3306","MI_WS1314","MIWS1314","Oq1gk28@").readDataBase();
+			DatabaseAdapter da = new DatabaseAdapter("85.25.155.25", "3306", "MI_WS1314", "MIWS1314", "Oq1gk28@");
+
+//			da.dumpToConsole();
+
+			SocialMessage[] messages = da .getTweets("tweets_sturm", new Timestamp(0), new Timestamp(System.currentTimeMillis()));
+			System.out.println(messages.length);
+			
+			
+//			messages = da .getTweets("tweets_sturm", new Timestamp(0), new Timestamp(0));
+//			System.out.println(messages.length);
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
