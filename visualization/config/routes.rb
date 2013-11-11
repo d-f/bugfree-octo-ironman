8db141652849f8ffdaf4e8c9ac30ec96280dc5b2
Lucas Jenss <lucas@x3ro.de>
2013-11-11 18:08:58 +0100

    Add settings controller and view

--- visualization/config/routes.rb
+++ visualization/config/routes.rb
@@ -56,6 +56,8 @@ Visualization::Application.routes.draw do
 
   root 'widget#index'
 
+  get 'settings' => 'settings#index', :as => :settings
+
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'
 end
