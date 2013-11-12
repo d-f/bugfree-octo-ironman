b31ff17a25274905c483bd1bf3c2242814a12d16
Lucas Jenss <lucas@x3ro.de>
2013-11-12 11:12:22 +0100

    Implement dashboard left menu

--- visualization/app/helpers/application_helper.rb
+++ visualization/app/helpers/application_helper.rb
@@ -4,4 +4,11 @@ module ApplicationHelper
     raw "<span class='fs1' aria-hidden='true' data-icon='#{icon}'></span>"
   end
 
+  def menu_items
+    [
+      [ t("Dashboard"), "&#xe0a2;", root_url ],
+      [ t("Settings"), "&#xe090;", settings_url ]
+    ]
+  end
+
 end
