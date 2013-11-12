0a155fbb89628d35779945cb75732df7aeb45e51
Stephan P <stephan.phieler@haw-hamburg.de>
2013-11-12 16:43:00 +0100

    added some console output

--- analysis/DataAnalysis/src/data/analysis/InformationExtractor.java
+++ analysis/DataAnalysis/src/data/analysis/InformationExtractor.java
@@ -19,23 +19,24 @@ public class InformationExtractor {
 		twitterData = new TwitterDataAnalyser();
 		twitterCategorizer = new Categorizer();
 
-		System.out.println("determine categories...");
+		System.out.print("determine categories...");
 		categories = twitterData.getCategories(TABLE_TWITTER_CATEGORIES);
+		System.out.println(" done.");
 
-		System.out.println("load training data...");
+		System.out.print("load training data...");
 		twitterTrainMessages = new SocialMessage[categories.length][TRAIN_MESSAGE_COUNT];
 		twitterTrainMessages = twitterData.getTrainMessages(TABLE_TWITTER_TRAINING, categories,
 				TRAIN_MESSAGE_COUNT);
+		System.out.println(" done.");
 
-		System.out.println("train classifier...");
+		System.out.print("train classifier...");
 		twitterCategorizer.train(categories, twitterTrainMessages);
-
-		System.out.println("...initialisation finished.");
+		System.out.println(" done.");
 		
-		System.out.println("save metadata...");
+		System.out.print("save metadata...");
 		// TODO method not yet ready 
 		//twitterData.storeMetadata();
-		
+		System.out.println(" done.");
 	}
 
 }
