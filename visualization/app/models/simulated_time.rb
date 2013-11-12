d19a638ec03f9fdbd3fe8a79191e19cb781c294f
Lucas Jenss <lucas@x3ro.de>
2013-11-12 13:27:43 +0100

    Implement SimulatedTime#now

--- visualization/app/models/simulated_time.rb
+++ visualization/app/models/simulated_time.rb
@@ -13,12 +13,18 @@ class SimulatedTime
       end
     end
 
+    def now
+      time = get()
+      DateTime.parse(time[0]) + (DateTime.now - DateTime.parse(time[1]))
+    end
+
     def set(time)
       time = DateTime.parse(time).to_s
       $redis.set("simulated_time", time)
       $redis.set("simulated_time_set_at", DateTime.now)
 
       [$redis.get("simulated_time"), $redis.get("simulated_time_set_at")]
+      TweetWorker.clear_last_execution
     end
 
 
