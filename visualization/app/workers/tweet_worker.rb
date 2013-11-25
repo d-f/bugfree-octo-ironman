bb4d2b59b602d99794de166270a2e88302a88d6a
Lucas Jenss <lucas@x3ro.de>
2013-11-25 17:31:41 +0100

    Refactor tweet information gathering to model

--- visualization/app/workers/tweet_worker.rb
+++ visualization/app/workers/tweet_worker.rb
@@ -12,17 +12,7 @@ class TweetWorker
     tweets = Tweet.includes(:categories, :information).where("timestamp >= ? and timestamp < ?", start, endd).to_a
     #tweets = [Tweet.includes(:categories, :information).first(:offset => rand(Tweet.count))] # for testing
     tweets = tweets.map do |t|
-      data = t.attributes
-
-      category = t.categories.first
-      data[:category_id] = category.try!(:id)
-      data[:category_name] = category.try!(:name)
-
-      data[:geolocation] = nilWhenEmpty(t.geolocation) || nilWhenEmpty(t.information.try!(:geolocation))
-      data[:geolocation] = geolocation_to_json(data[:geolocation])
-      data[:place] = nilWhenEmpty(t.place) || nilWhenEmpty(t.information.try!(:place))
-
-      data
+      t.enriched
     end
 
     if tweets.length > 0
@@ -30,22 +20,6 @@ class TweetWorker
     end
 
     TweetWorker.set_last_execution
-  end
-
-private
-
-  def nilWhenEmpty(x)
-    # rows in database are not real SQL NULL's but the string "null" -_-"
-    return nil if(x === "" || x === "null")
-    x
-  end
-
-  def geolocation_to_json(x)
-    if x =~ /.*{.*=([0-9.]+), .*=([0-9.]+)}/
-      {:latitude => $1, :longitude => $2}
-    else
-      x
-    end
   end
 
 
