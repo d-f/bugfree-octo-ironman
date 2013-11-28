5eeb54d7dfc29223e97e0f6d1f76976d35d7bbb3
denisfleischhauer <d.fleischhauer@gmx.de>
2013-11-28 10:59:05 +0100

    Felder als Schlüssel in Keys.java

---
+++ analysis/DataAnalysis/src/DBAdapter/Key.java
@@ -0,0 +1,25 @@
+package DBAdapter;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: denisf
+ * Date: 26.11.13
+ * Time: 12:02
+ * To change this template use File | Settings | File Templates.
+ */
+public class Key {
+    public static final String zc_id = "zc_id";
+    public static final String zc_loc_id = "zc_loc_id";
+    public static final String zc_location_name = "zc_location_name";
+    public static final String zc_lat = "zc_lat";
+    public static final String zc_lon = "zc_lon";
+    //geodb_type_names
+    public static final String type_id = "type_id";
+    public static final String type_locale = "type_locale";
+    public static final String name = "name";
+    //geodb_textdata
+    public static final String loc_id = "loc_id";
+    public static final String text_type = "text_type";
+    public static final String text_val = "text_val";
+
+}
--- analysis/DataAnalysis/src/DBAdapterImpl.java
+++ analysis/DataAnalysis/src/DBAdapterImpl.java
@@ -1,4 +1,5 @@
-import DBAdapter.Tables;
+import DBAdapter.*;
+import DBAdapter.Key;
 import data.analysis.SocialMessage;
 import org.jooq.*;
 import org.jooq.impl.DSL;
@@ -106,6 +107,8 @@ public class DBAdapterImpl {
         return result;
     }
 
+    //TODO  input:
+    //      output:
     public Map<String, String> getKoordinatenVonLocId(String locId) {
         open();
         Result result = create
@@ -127,13 +130,14 @@ public class DBAdapterImpl {
     public List<Map<String, Object>> getKoordinatenVonOrt(String ort){
         open();
         List<Map<String, Object>> result = create
-                .select(fieldByName("zip_coordinates","zc_location_name"),fieldByName("zip_coordinates","zc_lon"),fieldByName("zip_coordinates","zc_lat"))
+                .select(fieldByName("zip_coordinates", Key.zc_location_name),fieldByName("zip_coordinates","zc_lon"),fieldByName("zip_coordinates","zc_lat"))
                 .from(tableByName("zip_coordinates"))
                 .where(fieldByName("zip_coordinates","zc_location_name").eq(ort))
                 .groupBy(fieldByName("zip_coordinates","zc_location_name"))
                 .fetchMaps();
         System.out.println(result);
         close();
+
         return result;
     }
 
@@ -220,7 +224,7 @@ public class DBAdapterImpl {
 
     public List<Map<String, Object>> getInfoZurLocId(int loc_id) {
         open();
-        List<Map<String, Object>> result = create
+        Result<Record2<Object, Object>> result = create
                 .select(fieldByName("geodb_type_names","name"), fieldByName("geodb_textdata","text_val"))
                 .from(tableByName("geodb_textdata"))
                 .leftOuterJoin(tableByName("geodb_type_names"))
@@ -228,10 +232,10 @@ public class DBAdapterImpl {
                         .equal(fieldByName("geodb_type_names", "type_id")))
                 .where(fieldByName("loc_id")
                         .equal(loc_id))
-                .fetchMaps();
+                .fetch();
         close();
         System.out.println(result);
-        return result;
+        return null;
     }
 
     public List<Map<String, Object>> getLocIdZumOrt(String ort) {
